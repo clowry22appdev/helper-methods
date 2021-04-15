4a576f5a0da867a00052d4788220a041de253cf9
clowry22 <clowry@uchicago.edu>
2021-04-15 04:08:43 +0000

    currently working on edit form

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -39,10 +39,10 @@ class MoviesController < ApplicationController
   end
 
   def update
-    @movie = Movie.find(params.fetch(:id))
+    movie_attributes = params.fetch(:movie).permit(:title, :description)
+    @movie = Movie.find(params.fetch(:id))
+    @movie.update(movie_attributes)
 
-    @movie.title = params.fetch(:title)
-    @movie.description = params.fetch(:description)
 
     if @movie.valid?
       @movie.save
