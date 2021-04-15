a58bc976aac6ea4b88af7f30f47938c256b45604
clowry22 <clowry@uchicago.edu>
2021-04-15 20:16:28 +0000

    Created directors model and routes/refactored routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   # Routes for the Movie resource:
   resources :movies
+  resources :directors
+  
   # # CREATE
 
   # post "/movies" => "movies#create", as: :movies #movies_url and movies_path
