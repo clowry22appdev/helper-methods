32424bdfa61c203f01cbe35f298a19e86162314b
clowry22 <clowry@uchicago.edu>
2021-04-15 00:45:11 +0000

    Refactored routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,31 @@
 Rails.application.routes.draw do
-  get("/", { :controller => "movies", :action => "index" })
+  #get "/",  controller: "movies", action: "index" 
+  #get "/" => "movies#index" #short hand syntax
+  root "movies#index" #even shorter short hand syntax
 
   # Routes for the Movie resource:
 
   # CREATE
-  post("/movies", { :controller => "movies", :action => "create" })
-  get("/movies/new", { :controller => "movies", :action => "new" })
-          
+  #post("/movies", { :controller => "movies", :action => "create" })
+  post "/movies" => "movies#create"
+  # get("/movies/new", { :controller => "movies", :action => "new" })
+  get "/movies/new" => "movies#new"
+
   # READ
-  get("/movies", { :controller => "movies", :action => "index" })
-  get("/movies/:id", { :controller => "movies", :action => "show" })
+  # get("/movies", { :controller => "movies", :action => "index" })
+  get "/movies" => "movies#index"
+  # get("/movies/:id", { :controller => "movies", :action => "show" })
+  get "/movies/:id" => "movies#show"
   
   # UPDATE
-  patch("/movies/:id", { :controller => "movies", :action => "update" })
-  get("/movies/:id/edit", { :controller => "movies", :action => "edit" })
+  # patch("/movies/:id", { :controller => "movies", :action => "update" })
+  patch "/movies/:id" => "movies#update"
+  # get("/movies/:id/edit", { :controller => "movies", :action => "edit" })
+  get "/movies/:id/edit" => "movies#edit"
   
   # DELETE
-  delete("/movies/:id", { :controller => "movies", :action => "destroy" })
+  # delete("/movies/:id", { :controller => "movies", :action => "destroy" })
+  get "/movies/:id" => "movies#destroy"
 
   #------------------------------
 end
